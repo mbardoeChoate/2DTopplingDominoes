239e32a25111e0bfd3f5146af0cd632c50f6a443
mbardoe <mbardoe@gmail.com>
2022-04-20 08:02:08 -0400

    Working on Connected components

--- TwoDimTopplingDominoes.py
+++ TwoDimTopplingDominoes.py
@@ -6,6 +6,24 @@ class TwoDimTopplingDominoes:
     def __init__(self, moves):
         self.moves = moves
         self._value = None
+        self.find_connected_components()
+
+    def find_connected_components(self):
+        self.components = []
+        moves = self.moves.copy()
+        current_component = []
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        for move in moves:
+            current_component.append(move)
+            for move in current_component:
+                for direction in directions:
+                    neighbor = (move[0] + direction[0], move[1] + direction[1])
+                    if neighbor in self.moves and not neighbor in current_component:
+                        current_component.append(neighbor)
+                        if neighbor in moves:
+                            moves.remove(neighbor)
+            self.components.append(current_component.copy())
+            current_component = []
 
     def move(self, move, direction):
         if move not in self.moves:
@@ -26,15 +44,21 @@ class TwoDimTopplingDominoes:
             return self._value
 
         else:
-            if len(self.moves) == 0:
-                return 0
+            if len(self.components) > 1:
+                value = 0
+                for component in self.components:
+                    subgame = TwoDimTopplingDominoes(component)
+                    value = value ^ subgame.get_value()
             else:
-                subgames = []
-                for move in self.moves:
-                    for direction in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                        subgame = self.move(move, direction)
-                        subgames.append(subgame.get_value())
-                self._value = mex(subgames)
+                if len(self.moves) == 0:
+                    return 0
+                else:
+                    subgames = []
+                    for move in self.moves:
+                        for direction in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                            subgame = self.move(move, direction)
+                            subgames.append(subgame.get_value())
+                    self._value = mex(subgames)
         return self._value
 
     @classmethod
@@ -53,3 +77,8 @@ class TwoDimTopplingDominoes:
         for j in range(right):
             moves.append((j + 1, 0))
         return TwoDimTopplingDominoes(moves)
+
+
+if __name__ == "__main__":
+    x = TwoDimTopplingDominoes([(0, 0), (0, 1), (2, 0), (2, 1)])
+    print(x.components)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from TwoDimTopplingDominoes import TwoDimTopplingDominoes
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    game = TwoDimTopplingDominoes.generateL(1, 4)
+    game = TwoDimTopplingDominoes.generateL(3, 6)
     print(str(game.get_value()))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
